ff59756927edd84571bad93b6107e7192b69a959
Yuliia Laba <yuliia.laba-ip213@nung.edu,ua>
2025-06-01 00:34:29 +0300

    Initial commit with unit tests

---
+++ sample.js
@@ -0,0 +1,8 @@
+export const fromCharCode = code =>
+String.fromCharCode(code)
+
+export const padStart = (str, targetLength, padString) =>
+str.padStart(targetLength, padString)
+
+export const padEnd = (str, targetLength, padString) =>
+str.padEnd(targetLength, padString)
