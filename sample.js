a98a654e6ee0c79d68934b0daf5066ff70e06021
Yuliia Laba <yuliia.laba-ip213@nung.edu,ua>
2025-06-01 02:05:23 +0300

    Initial commit

--- sample.js
+++
@@ -1,8 +0,0 @@
-export const fromCharCode = code =>
-String.fromCharCode(code)
-
-export const padStart = (str, targetLength, padString) =>
-str.padStart(targetLength, padString)
-
-export const padEnd = (str, targetLength, padString) =>
-str.padEnd(targetLength, padString)
